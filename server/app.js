292445f136e4becab09362a6739e5385034a650f
Víctor Peña Romero <vic.pero@icloud.com>
2020-04-23 20:52:09 -0500

    first commit

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const graphqlHTTP =require('express-graphql')
+const schema = require('./schema/schema')
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+    schema
+}))
+
+app.listen(4000, () => {
+    console.log('Now listening for requests on port 4000');
+})
