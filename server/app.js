e6516dcc4bdc25dd23be8e6c554d76cd6d1a8328
Víctor Peña Romero <vic.pero@icloud.com>
2020-04-24 16:57:55 -0500

    add dummy data

--- server/app.js
+++ server/app.js
@@ -5,9 +5,10 @@ const schema = require('./schema/schema')
 const app = express()
 
 app.use('/graphql', graphqlHTTP({
-    schema
+    schema,
+    graphiql: true
 }))
 
 app.listen(4000, () => {
-    console.log('Now listening for requests on port 4000');
+    console.log('Now listening for requests on port 4000')
 })
