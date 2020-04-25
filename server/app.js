d5cf6896a77afbec5df0b50ddb3ad1676857dfa0
Víctor Peña Romero <vic.pero@icloud.com>
2020-04-25 14:17:19 -0500

    add connection to db

--- server/app.js
+++ server/app.js
@@ -1,8 +1,23 @@
+require('dotenv').config()
+
 const express = require('express')
 const graphqlHTTP =require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose
+    .connect(process.env.DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(x => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch(err => {
+        console.error('Error connecting to mongo', err)
+    })
 
 app.use('/graphql', graphqlHTTP({
     schema,
